716af21bcf0ebda18e50b3e6a2e0ac5d17839c6d
Priyanka7081 <priyankau7081@gmail.com>
2026-04-10 16:57:13 +0530

    project

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import "dotenv/config";
 import connectDB from "./database/db.js";
 import userRoute from "./routes/userRoute.js";
+import cors from 'cors'
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -10,6 +11,10 @@ connectDB();
 
 //middleware
 app.use(express.json());
+app.use(cors({
+  origin:'http://localhost:5173',
+  credentials:true
+}))
 
 app.use("/api/v1/user", userRoute);
 
